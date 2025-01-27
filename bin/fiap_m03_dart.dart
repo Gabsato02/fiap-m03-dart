fd31070c46f4ff638fbd587cb3a9d1684d6e890e
Gabriel <gabsato02@gmail.com>
2025-01-27 16:29:31 -0300

    Commit inicial

---
+++ bin/fiap_m03_dart.dart
@@ -0,0 +1,5 @@
+import 'package:fiap_m03_dart/fiap_m03_dart.dart' as fiap_m03_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${fiap_m03_dart.calculate()}!');
+}
---
+++ test/fiap_m03_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:fiap_m03_dart/fiap_m03_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
