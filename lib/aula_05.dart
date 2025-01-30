63558c6d5a210710eed4c49cc9b52ce6b6830fd4
Gabriel <gabsato02@gmail.com>
2025-01-30 18:23:55 -0300

    Adiciona interceptor

--- lib/aula_05.dart
+++ lib/aula_05.dart
@@ -1,11 +1,25 @@
-// REQUISIÇÕES HTTP
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
 
+// INTERCEPTOR
+class ClientInterceptor extends http.BaseClient {
+  final http.Client _client = http.Client();
+
+  @override
+  Future<http.StreamedResponse> send(http.BaseRequest request) {
+    request.headers['accept'] = 'application/json';
+    request.headers['Content-Type'] = 'application/json';
+
+    return _client.send(request);
+  }
+}
+
+// REQUISIÇÃO
 Future<void> fetchData(String cep) async {
   try {
-    final response = await http.get(Uri.parse('https://viacep.com.br/ws/$cep/json'));
+    final client = ClientInterceptor();
+
+    final response = await client.get(Uri.parse('https://viacep.com.br/ws/$cep/json'));
 
     var data = jsonDecode(response.body);
 
